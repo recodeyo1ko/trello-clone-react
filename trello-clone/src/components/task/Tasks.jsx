9ad286c6313cd7df7eec40b656050b1bde31d996
recodeyo1ko <kuronekoryohei@gmail.com>
2023-10-23 00:38:52 +0900

    削除機能

--- trello-clone/src/components/task/Tasks.jsx
+++ trello-clone/src/components/task/Tasks.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Task } from "./Task";
 
-export const Tasks = ({ inputText, taskList }) => {
+export const Tasks = ({ inputText, taskList, setTaskList }) => {
   console.log(taskList);
   return (
     <div>
       {taskList.map((task) => (
         <div>
-          <Task task={task} />
+          <Task task={task} taskList={taskList} setTaskList={setTaskList} />
         </div>
       ))}
     </div>
