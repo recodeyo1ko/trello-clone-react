c5075cc586a048d8eb659a13816a00908123b59b
recodeyo1ko <kuronekoryohei@gmail.com>
2023-10-29 18:12:05 +0900

    D&D機能の導入、順番は未保持

--- trello-clone/src/components/task/Tasks.jsx
+++ trello-clone/src/components/task/Tasks.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import { Task } from "./Task";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 export const Tasks = ({ inputText, taskList, setTaskList }) => {
-  console.log(taskList);
   return (
     <div>
-      {taskList.map((task) => (
-        <div>
-          <Task task={task} taskList={taskList} setTaskList={setTaskList} />
-        </div>
-      ))}
+      <DragDropContext>
+        <Droppable droppableId="droppable">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {taskList.map((task) => (
+                <div key={task.id}>
+                  <Task
+                    task={task}
+                    taskList={taskList}
+                    setTaskList={setTaskList}
+                  />
+                </div>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 };
